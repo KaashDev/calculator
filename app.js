f953b019294b3a2db0b7eb39512aecf73de3d930
KaashDev <seetohulkaash@gmail.com>
2026-05-21 13:43:40 -0400

    basic operations

--- app.js
+++ app.js
@@ -5,4 +5,17 @@
         - multiply
         - divide*/
 
+const add = function(a, b){
+    return a+b;
+}
 
+const subtract = function(a, b){
+    return a - b;
+}
+
+const multiply = function(a, b){
+    return a * b;
+}
+const divide = function(a, b){
+    return a/b;
+}
