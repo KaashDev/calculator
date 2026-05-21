daebdd9543109bb2105f3228500e29b8fbca31b9
KaashDev <seetohulkaash@gmail.com>
2026-05-21 16:42:54 -0400

    Made functionality more efficient

--- app.js
+++ app.js
@@ -50,81 +50,12 @@ if (typeof module !== 'undefined' && module.exports) {
     };
 }
 
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero");
-
-const addSymbol = document.querySelector("#add");
-const subtractSymbol = document.querySelector("#subtract");
-const multiplySymbol = document.querySelector("#multiply");
-const divideSymbol = document.querySelector("#divide");
 const clear = document.querySelector("#clear");
-
 const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll(".button");
 
-one.addEventListener("click", ()=>{
-    screen.textContent += "1";
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+    screen.textContent += button.textContent;
 });
-
-two.addEventListener("click", ()=>{
-    screen.textContent += "2";
-});
-
-three.addEventListener("click", ()=>{
-    screen.textContent += "3";
-});
-
-four.addEventListener("click", ()=>{
-    screen.textContent += "4";
-});
-
-five.addEventListener("click", ()=>{
-    screen.textContent += "5";
-});
-
-six.addEventListener("click", ()=>{
-    screen.textContent += "6";
-});
-
-seven.addEventListener("click", ()=>{
-    screen.textContent += "7";
-});
-
-eight.addEventListener("click", ()=>{
-    screen.textContent += "8";
-});
-
-nine.addEventListener("click", ()=>{
-    screen.textContent += "9";
-});
-
-zero.addEventListener("click", ()=>{
-    screen.textContent += "0";
-});
-
-clear.addEventListener("click", ()=>{
-    screen.textContent = "";
-});
-
-addSymbol.addEventListener("click", ()=>{
-    screen.textContent += " + ";
-});
-
-subtractSymbol.addEventListener("click", ()=>{
-    screen.textContent += " - ";
-});
-
-multiplySymbol.addEventListener("click", ()=>{
-    screen.textContent += " x ";
-});
-
-divideSymbol.addEventListener("click", ()=>{
-    screen.textContent += " ÷ ";
 });
